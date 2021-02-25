6c8214739a7c2ed11f46ad3aebe083d9c0bfa8d2
Werner Luiz Gottschalt <werner@lumiun.com>
2021-02-25 15:37:00 -0300

    Aula 1 - Calculando a media

---
+++ Aula 1 - notas.php
@@ -0,0 +1,16 @@
+<?php
+
+$notaPortugues = 9;
+$notaMatematica = 3;
+$notaGeografia = 10;
+$notaHistoria = 5;
+$notaQuimica = 10;
+
+$media = ($notaPortugues + $notaMatematica + $notaGeografia + $notaHistoria + $notaQuimica) / 5;
+
+echo "A nota de português é: $notaPortugues <br>";
+echo "A nota de matemática é: $notaMatematica <br>";
+echo "A nota de geografia é: $notaGeografia <br>";
+echo "A nota de história é: $notaHistoria <br>";
+echo "A nota de quimica é: $notaQuimica <br>";
+echo "A média é: $media <br>";
